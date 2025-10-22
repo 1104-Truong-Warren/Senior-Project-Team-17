d7aed064aeba45d7cbf1d9352b662e10ce5f9bd6
Ellison Domingo <ellisond@nevada.unr.edu>
2025-10-22 16:30:29 -0700

    Completed GridBehavior.cs according to video
    Added "using System.Collections.Generic" so Lists work
    Added bool findDistance
    Added path List
    Added code to Update function so path calculation runs when findDistance toggled on in Unity inspector
    Created SetDistance function
    Created SetPath function
    Created TestFourDirections function
    Created SetVisited function
    Created FindClosest function
    Corrected function name typo (InitalSetup -> InitialSetup)
    Corrected small logic issue in InitialSetup function (x + 1 to x - 1)

--- Blackout Phase/Assets/Scripts/GridBehavior.cs
+++ Blackout Phase/Assets/Scripts/GridBehavior.cs
@@ -1,7 +1,12 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GridBehavior : MonoBehaviour
 {
+    // Ellison
+    // likely not necessary for functionality, just set here and toggled using Unity inspector to trigger distance finding on and off (see Update function)
+    public bool findDistance = false;
 
     //setting variables for grid size, should be set using the in-engine inspector
     //this will necesitate that all maps are technically rectangular, but we can mark out map boundaries with inaccessible tiles
@@ -22,7 +27,12 @@ public class GridBehavior : MonoBehaviour
     public int startY = 0; 
     // Warren | the ending coordinates of the grid
     public int endX = 2; 
-    public int endY = 2; 
+    public int endY = 2;
+
+
+    // Ellison
+    // This path is the running list for the most optimal route to the destination
+    public List<GameObject> path = new List<GameObject>();
 
 
     //when activated, generate the grid based on the GenerateGrid function
@@ -40,11 +50,16 @@ public class GridBehavior : MonoBehaviour
         }
     }
 
-
+    // Ellison | added code to update
     // Update is called once per frame
     void Update()
     {
-        
+        if (findDistance)
+        {
+            SetDistance();
+            SetPath();
+            findDistance = false;
+        }
     }
 
 
@@ -66,8 +81,75 @@ public class GridBehavior : MonoBehaviour
             }
         }
     }
+
+    // Ellison
+    void SetDistance()
+    {
+        InitialSetup();
+        int x = startX;
+        int y = startY;
+        int[] testArray = new int[rows * columns]; // creates an array larger than any possible movement
+        for (int step = 1; step < rows * columns; step++)
+        {
+            foreach (GameObject obj in gridArray) // for each tile in the grid array
+            {
+                if (obj && obj.GetComponent<GridStat>().visited == step - 1) // if current tile's visited status is equal to step - 1
+                {
+                    TestFourDirections(obj.GetComponent<GridStat>().x, obj.GetComponent<GridStat>().y, step); // then set valid adjacent tiles' visited to step
+                }
+            }
+        }
+    }
+
+    // Ellison
+    void SetPath()
+    {
+        int step;
+        int x = endX;
+        int y = endY;
+        List<GameObject> tempList = new List<GameObject>();
+        path.Clear();
+
+        if (gridArray[endX, endY] && gridArray[endX, endY].GetComponent<GridStat>().visited > 0)
+        {
+            path.Add(gridArray[x, y]);
+            step = gridArray[x, y].GetComponent<GridStat>().visited - 1; // traversing backwards
+        }
+        else
+        {
+            print("Can't reach the desired location.");
+            return;
+        }
+
+        for (int i = step; step > -1; step--) // start at last spot and go down to 0
+        {
+            if (TestDirection(x, y, step, 1)) // testing up
+            {
+                tempList.Add(gridArray[x, y + 1]);
+            }
+            if (TestDirection(x, y, step, 2)) // testing right
+            {
+                tempList.Add(gridArray[x + 1, y]);
+            }
+            if (TestDirection(x, y, step, 3)) // testing down
+            {
+                tempList.Add(gridArray[x, y - 1]);
+            }
+            if (TestDirection(x, y, step, 4)) // testing left
+            {
+                tempList.Add(gridArray[x - 1, y]);
+            }
+
+            GameObject tempObj = FindClosest(gridArray[endX, endY].transform, tempList); // gets closest object
+            path.Add(tempObj); // adds it to path
+            x = tempObj.GetComponent<GridStat>().x; // updates x and y to that object's coordinates
+            y = tempObj.GetComponent<GridStat>().y;
+            tempList.Clear(); // clear tempList for next iteration
+        }
+    }
+
     // Warren | function that sets up the grid so we could label the points.
-    void InitalSetup()
+    void InitialSetup()
     {
         // Warren | goes through every GameObject in the grid and resets its visited status to -1, and 0 is the starting point.
         foreach (GameObject obj in gridArray)
@@ -118,7 +200,7 @@ public class GridBehavior : MonoBehaviour
                 
             // Warren | checks whether it is moving left
             case 4:
-                if (x - 1 > -1 && gridArray[x + 1, y] && gridArray[x - 1, y].GetComponent<GridStat>().visited == step)
+                if (x - 1 > -1 && gridArray[x - 1, y] && gridArray[x - 1, y].GetComponent<GridStat>().visited == step)
                 {
                     return true;
                 }
@@ -130,5 +212,55 @@ public class GridBehavior : MonoBehaviour
             default:
                 return false;
         }
+    }
+
+
+    // Ellison
+    // function tests all four directions for valid movement and sets values accordingly
+    void TestFourDirections(int x, int y, int step)
+    {
+        if (TestDirection(x, y, -1, 1)) // -1 step in 1 direction (up)
+        {
+            SetVisited(x, y + 1, step);
+        }
+        if (TestDirection(x, y, -1, 2)) // -1 step in 2 direction (right)
+        {
+            SetVisited(x + 1, y, step);
+        }
+        if (TestDirection(x, y, -1, 3)) // -1 step in 3 direction (down)
+        {
+            SetVisited(x, y - 1, step);
+        }
+        if (TestDirection(x, y, -1, 4)) // -1 step in 4 direction (left)
+        {
+            SetVisited(x - 1, y, step);
+        }
+    }
+
+    // Ellison
+    // function sets "visited" value of a tile at coordinates (x, y) to step parameter
+    void SetVisited(int x, int y, int step)
+    {
+        if (gridArray[x, y])
+        {
+            gridArray[x, y].GetComponent<GridStat>().visited = step;
+        }
+    }
+
+    // Ellison
+    // function finds the shortest distance from targetLocation to any object in the list and returns that object
+    GameObject FindClosest(Transform targetLocation, List<GameObject> list)
+    {
+        float currentDistance = scale * rows * columns; // arbitrarily large distance
+        int indexNumber = 0;
+        for (int i = 0; i < list.Count; i++) // for each object in list
+        {
+            if (Vector3.Distance(targetLocation.position, list[i].transform.position) < currentDistance) // if shorter distance found
+            {
+                currentDistance = Vector3.Distance(targetLocation.position, list[i].transform.position); // replace with new shortest distance
+                indexNumber = i; // update indexNumber to this current one
+            }
+        }
+        return list[indexNumber];
     }
 }
