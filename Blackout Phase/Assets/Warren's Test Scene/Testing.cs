32bfd464c71e815591740eab11fd42a80324a8a1
1104-Truong-Warren <warrentruong2310@mail.com>
2025-11-27 10:29:34 -0800

    Creating own grid
    Experimenting new grid

---
+++ Blackout Phase/Assets/Warren's Test Scene/Testing.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private void Start()
+    {
+        NewGrid grid = new NewGrid(4,2, 10f);
+    }
+}
