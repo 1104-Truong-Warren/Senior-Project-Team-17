3ac74ebaa6edab478023d7dc20730024fc2a97e7
1104-Truong-Warren <warrentruong2310@mail.com>
2025-11-28 10:52:19 -0800

    Working on fixing pathfinding

--- Blackout Phase/Assets/Warren's Test Scene/Testing.cs
+++ Blackout Phase/Assets/Warren's Test Scene/Testing.cs
@@ -48,9 +48,9 @@ public class Testing : MonoBehaviour
             List<PathNode> path = pathfinding.FindPath(0, 0, x, y);
             if(path != null)
             {
-                for(int i=0; i<path.Count - 1; i++)
+                for (int i = 0; i < path.Count - 1; i++)
                 {
-                    Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 10f + Vector3.one * 5f, new Vector3(path[i+1].x, path[i+1].y) * 10f + Vector3.one * 5f);
+                    Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 10f + Vector3.one * 5f, new Vector3(path[i+1].x, path[i+1].y) * 10f + Vector3.one * 5f, Color.green, 5f);
                 }
             }
         }
