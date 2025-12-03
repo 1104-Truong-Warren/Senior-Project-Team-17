9246b7ac67cc8ed62c380338e9da3efadcfcc1dc
Jason_Kimoto <jasonkimoto2243@gmail.com>
2025-12-02 18:53:13 -0800

    created player controller script
    created player controller script w/ associated values, like the basic enemy class in EnemyController branch, please ask Jason if guidance is needed

---
+++ Blackout Phase/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int currentX;
+    public int currentY;
+    public int health;
+
+    //pathfinding
+    public Grid grid;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
