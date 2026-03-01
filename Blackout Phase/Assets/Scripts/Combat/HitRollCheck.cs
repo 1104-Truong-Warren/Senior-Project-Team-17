c63759948194e1109e9a9251e8e165913c6ba25a
OoZinniaoO <longzhiwu1221@gmail.com>
2026-03-01 00:38:14 -0800

    merge fix

--- Blackout Phase/Assets/Scripts/Combat/HitRollCheck.cs
+++ Blackout Phase/Assets/Scripts/Combat/HitRollCheck.cs
@@ -22,7 +22,7 @@ public static class HitRollCheck
         return Mathf.Clamp(hitChance, min, max); // return the hit Chance, prevent out of range it has to be in 1 - 100 range
     }
 
-    public static int FinalCritChanceCal(int baseCrit, int skillCritB, int min = 10, int max = 100)
+    public static int FinalCritChanceCal(int baseCrit, int skillCritB, int min = 5, int max = 100)
     {
         return Mathf.Clamp((baseCrit + skillCritB), min, max); // return the base crit + skill crit, has to be in 0 - 100 range
     }
