e0a57c122eaa8d2e5ff1f9019b692e11f25d570e
Ellison Domingo <ellisond@nevada.unr.edu>
2026-04-28 19:35:56 -0700

    Inventory Overhaul (again)
    Created InventoryData.cs; scriptable object to hold current inventory and equipment independent of hierarchy objects
    Moved some functionality out of Inventory.cs and EquipmentManager.cs to use the scriptable object instead
    Small changes to InventoryUI.cs, EquipmentUI.cs, and Equipment.cs to better interact with the scriptable object
    Inventory and Equipment now stays across levels and scene loads because the managers now just reference the scriptable object
    
    Added some God Mode items to max out attack and HP for testing purposes only (add to inventory with >= 3 open slots with Ctrl + G)

--- Blackout Phase/Assets/Scripts/Inventory/Inventory.cs
+++ Blackout Phase/Assets/Scripts/Inventory/Inventory.cs
@@ -7,15 +7,10 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     public static Inventory instance;
+    public InventoryData data;
 
     void Awake()
     {
-        if (instance != null)
-        {
-            Debug.LogWarning("More than one instance of Inventory found!");
-            return;
-        }
-        
         instance = this;
     }
 
@@ -28,32 +23,33 @@ public class Inventory : MonoBehaviour
 
     public List<Item> items = new List<Item>();
 
+    [Header("God Mode Items")]
+    public Item godModeItem1;
+    public Item godModeItem2;
+    public Item godModeItem3;
+
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(KeyCode.G))
+        {
+            GetGodModeItems();
+        }
+    }
     public void Add(Item item)
     {
-        if (items.Count >= maxItems)
-        {
-            Debug.Log("Inventory is full. Cannot add " + item.itemName);
-            return;
-        }
-        else
-        {
-            items.Add(item);
-            Debug.Log("Added " + item.itemName + " to inventory.");
-            if (onItemChangedCallback != null)
-            {
-                onItemChangedCallback.Invoke();
-            }
-            return;
-        }
+        data.Add(item);
     }
 
     public void Remove (Item item)
     {
-        items.Remove(item);
-        Debug.Log("Removed " + item.itemName + " from inventory.");
-        if (onItemChangedCallback != null)
-        {
-            onItemChangedCallback.Invoke();
-        }
+        data.Remove(item);
+    }
+
+    public void GetGodModeItems()
+    {
+        Add(godModeItem1);
+        Add(godModeItem2);
+        Add(godModeItem3);
+        Debug.Log("God Mode Items added to inventory.");
     }
 }
